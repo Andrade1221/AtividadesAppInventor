1143c19ef48024a66e29d088c3413f9b9f6f3741
Andrade1221 <111130558+Andrade1221@users.noreply.github.com>
2022-11-23 10:19:13 -0300

    Add files via upload

---
+++ Ativ.lua
@@ -0,0 +1,23 @@
+1 - Qual o sistema operacional mais usado no mundo na data atual (2022) considerando todos os dispositivos moveis e desktops?
+R: Android (44,66%).
+
+2 - O que é MIT APP Inventor e na sua opinião, qual sua importancia?
+R: O MIT App Inventor é uma ferramenta de programação baseada em
+blocos que permite que qualquer um, mesmo iniciantes, comecem a programar
+e construir aplicativos totalmente funcionais para dispositivos Android.
+
+R: Sua importancia e o desenvolvimento e aprimoramento do mundo tecnologico, pois ele sendo bem simples de usar qualquer um pode por sua criatividade em pratica.
+
+3 - O Android e um sistema operacional que roda sobre qual kernel?
+R: O sistema Android consiste em um Kernel baseado no Kernel Linux, especificamente do ramo (LTS), suporte a longo tempo.
+
+4 - Quais as 3 principais linguagens de programação oficiais para programação de APPS Androids nativos ?
+R:  1. Java
+    2. Kotlin
+    3. C++
+    4. C#
+    5. Lua
+    6. Basic
+
+5 - Qual a extenção de arquivo para aplicativos Android?
+R: .apk - APK é a sigla para Android Application Pack.
